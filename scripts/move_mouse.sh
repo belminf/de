66a5fb12c2d5cf875bde8a606e8ae22e142610a1
Belmin Fernandez <belminf@gmail.com>
2020-08-22 09:28:55 -0400

    Fixes mouse moving issues

--- scripts/move_mouse.sh
+++ scripts/move_mouse.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/bash
 
 # Get current window ID
-CURRENT_WIN=$(xdotool getactivewindow)
+#eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
 
-# Get geometry details
-#
-# Example output:
-#
-# WINDOW=37748739
-# X=978
-# Y=18
-# WIDTH=942
-# HEIGHT=1062
-# SCREEN=0
-eval $(xdotool getwindowgeometry --shell $CURRENT_WIN)
+#CURRENT_WIN=$(xdotool getactivewindow)
 
-# Get middle offsets
-MID_WIDTH=$(expr $WIDTH / 2)
-MID_HEIGHT=$(expr $HEIGHT / 2)
+## Get geometry details
+##
+## Example output:
+##
+## WINDOW=37748739
+## X=978
+## Y=18
+## WIDTH=942
+## HEIGHT=1062
+## SCREEN=0
+#eval $(xdotool getwindowgeometry --shell $CURRENT_WIN)
 
-# Move the cursor
-xdotool mousemove -w $CURRENT_WIN $MID_WIDTH $MID_HEIGHT
+## Get middle offsets
+#MID_WIDTH=$(expr $WIDTH / 2)
+#MID_HEIGHT=$(expr $HEIGHT / 2)
+
+## Move the cursor
+#xdotool mousemove -w $CURRENT_WIN $MID_WIDTH $MID_HEIGHT
+
+eval "$(xdotool getwindowfocus getwindowgeometry --shell)"
+xdotool mousemove "$((X + WIDTH / 2))" "$((Y + HEIGHT / 2))"
